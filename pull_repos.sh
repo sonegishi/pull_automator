2215ad4b8a30d472207f96e8559cd5323e370d6f
sonegishi <5055ngs@gmail.com>
2018-09-22 04:00:17 +0100

    Go back to an original branch

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,10 +1,11 @@
 #!/usr/local/bin/zsh
 # This assumes all repositories in the local pc is cloned using SSH
 
-for repo in `\find /Users/SO/GitHub -name .git`; do
+for repo in `\find $HOME/GitHub -name .git`; do
   echo ""
   echo "****** Getting latest for" ${repo} "******"
   cd "${repo}" && cd .. && git co master && git pull
   echo "******************************************"
+  git co -
 done
 
